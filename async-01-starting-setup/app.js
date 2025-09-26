835424b77d9637959c10a3cb711b7f596b732b3a
MINJI <always0524@naver.com>
2025-09-26 23:57:49 +0900

    learn. 섹션17-동기코드(Sync Code) - Promise

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -1,21 +1,30 @@
 const button = document.querySelector("button");
 const output = document.querySelector("p");
 
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, duration);
+  });
+  return promise;
+};
+
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
     (posData) => {
-      setTimeout(() => {
-        console.log(posData);
-      }, 2000);
+      setTimer(2000).then((data) => {
+        console.log(data, posData);
+      });
     },
     (error) => {
       console.log(error);
     }
   );
   setTimeout(() => {
-    console.log("Timer done!");
+    console.log("Timer done");
   }, 0);
-  console.log("Getting positon...");
+  console.log("Getting position");
 }
 
 button.addEventListener("click", trackUserHandler);
