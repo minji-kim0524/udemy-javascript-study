8d04002db8d679583a380597f0c8acd0d7410c97
MINJI <always0524@naver.com>
2025-10-03 23:00:42 +0900

    learn. 섹션17-동기코드(Sync Code) - Promise 오류 처리

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -7,7 +7,9 @@ const getPosition = (opts) => {
       (success) => {
         resolve(success);
       },
-      (error) => {},
+      (error) => {
+        reject(error);
+      },
       opts
     );
   });
@@ -28,7 +30,11 @@ function trackUserHandler() {
   getPosition()
     .then((posData) => {
       positionData = posData;
-      return setTimer();
+      return setTimer(2000);
+    })
+    .catch((err) => {
+      console.log(err);
+      return "on we go...";
     })
     .then((data) => {
       console.log(data, positionData);
