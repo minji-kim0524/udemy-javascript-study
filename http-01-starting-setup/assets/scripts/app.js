7749bd5f50051dc8324321883cf752fb572710f3
MINJI <always0524@naver.com>
2025-10-05 10:23:56 +0900

    learn. 섹션18-HTTP 요청 - GET 메서드 프로미스화

--- http-01-starting-setup/assets/scripts/app.js
+++ http-01-starting-setup/assets/scripts/app.js
@@ -2,24 +2,61 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-const xhr = new XMLHttpRequest();
+function sendHttpRequest(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-// 요청구성 1단계
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+    // 요청구성 1단계
+    xhr.open(method, url);
 
-xhr.responseType = "json";
+    xhr.responseType = "json";
 
-xhr.onload = function () {
-  //   const listOfPosts = JSON.parse(xhr.response);
-  const listOfPosts = xhr.response;
-  //   console.log(listOfPosts);
-  for (const post of listOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector("h2").textContent = post.title.toUpperCase();
-    postEl.querySelector("p").textContent = post.body;
-    listElement.append(postEl);
-  }
-};
+    xhr.onload = function () {
+      resolve(xhr.response);
+      //   const listOfPosts = JSON.parse(xhr.response);
+      //   const listOfPosts = xhr.response;
+      //   console.log(listOfPosts);
+      //   for (const post of listOfPosts) {
+      //     const postEl = document.importNode(postTemplate.content, true);
+      //     postEl.querySelector("h2").textContent = post.title.toUpperCase();
+      //     postEl.querySelector("p").textContent = post.body;
+      //     listElement.append(postEl);
+      //   }
+    };
 
-// 요청 전송
-xhr.send();
+    // 요청 전송
+    xhr.send();
+  });
+  return promise;
+}
+
+function fetchPosts() {
+  sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/posts").then(
+    (responseData) => {
+      const listOfPosts = responseData;
+      for (const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector("h2").textContent = post.title.toUpperCase();
+        postEl.querySelector("p").textContent = post.body;
+        listElement.append(postEl);
+      }
+    }
+  );
+}
+
+// async / await
+// async function fetchPosts() {
+//   const responseData = await sendHttpRequest(
+//     "GET",
+//     "https://jsonplaceholder.typicode.com/posts"
+//   );
+//   const listOfPosts = responseData;
+//   for (const post of listOfPosts) {
+//     const postEl = document.importNode(postTemplate.content, true);
+//     postEl.querySelector("h2").textContent = post.title.toUpperCase();
+//     postEl.querySelector("p").textContent = post.body;
+//     listElement.append(postEl);
+//   }
+// }
+
+fetchPosts();
