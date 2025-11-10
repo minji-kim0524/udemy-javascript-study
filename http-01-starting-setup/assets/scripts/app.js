2741c79d2bfe7600148ae76ca0d1d973ea695993
MINJI <always0524@naver.com>
2025-11-11 08:49:15 +0900

    learn. 섹션18-HTTP 요청 - POST 요청으로 데이터 보내기

--- http-01-starting-setup/assets/scripts/app.js
+++ http-01-starting-setup/assets/scripts/app.js
@@ -2,7 +2,7 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-function sendHttpRequest(method, url) {
+function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
 
@@ -25,23 +25,48 @@ function sendHttpRequest(method, url) {
     };
 
     // 요청 전송
-    xhr.send();
+    xhr.send(JSON.stringify(data));
   });
   return promise;
 }
 
-function fetchPosts() {
-  sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/posts").then(
-    (responseData) => {
-      const listOfPosts = responseData;
-      for (const post of listOfPosts) {
-        const postEl = document.importNode(postTemplate.content, true);
-        postEl.querySelector("h2").textContent = post.title.toUpperCase();
-        postEl.querySelector("p").textContent = post.body;
-        listElement.append(postEl);
-      }
-    }
+// function fetchPosts() {
+//   sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/posts").then(
+//     (responseData) => {
+//       const listOfPosts = responseData;
+//       for (const post of listOfPosts) {
+//         const postEl = document.importNode(postTemplate.content, true);
+//         postEl.querySelector("h2").textContent = post.title.toUpperCase();
+//         postEl.querySelector("p").textContent = post.body;
+//         listElement.append(postEl);
+//       }
+//     }
+//   );
+// }
+
+async function fetchPosts() {
+  const responseData = await sendHttpRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
   );
+  const listOfPosts = responseData;
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector("h2").textContent = post.title.toUpperCase();
+    postEl.querySelector("p").textContent = post.body;
+    listElement.append(postEl);
+  }
+}
+
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId,
+  };
+
+  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", data);
 }
 
 // async / await
@@ -60,3 +85,4 @@ function fetchPosts() {
 // }
 
 fetchPosts();
+createPost("DUMMY", "A dummy post!");
