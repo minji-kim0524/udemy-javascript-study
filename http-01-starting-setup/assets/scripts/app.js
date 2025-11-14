42a2e3aeeb6db5061945953fd11b95a660ca9800
MINJI <always0524@naver.com>
2025-11-14 21:49:56 +0900

    learn. fetch 오류 처리하기

--- http-01-starting-setup/assets/scripts/app.js
+++ http-01-starting-setup/assets/scripts/app.js
@@ -38,9 +38,21 @@ function sendHttpRequest(method, url, data) {
     headers: {
       "Content-type": "application/json",
     },
-  }).then((response) => {
-    return response.json();
-  });
+  })
+    .then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        return response.json();
+      } else {
+        return response.json().then((errData) => {
+          console.log(errData);
+          throw new Error("Something went wrong-server-side");
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      throw new Error("Something went wrong!");
+    });
 }
 
 // function fetchPosts() {
