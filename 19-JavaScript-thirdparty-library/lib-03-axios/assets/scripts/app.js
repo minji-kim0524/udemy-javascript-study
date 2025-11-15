2bac61797137e9381b30ba4d651c83f1e8bb8299
MINJI <always0524@naver.com>
2025-11-15 20:44:39 +0900

    learn. axios 라이브러리 사용하기

--- 19-JavaScript-thirdparty-library/lib-03-axios/assets/scripts/app.js
+++ 19-JavaScript-thirdparty-library/lib-03-axios/assets/scripts/app.js
@@ -1,8 +1,8 @@
-const listElement = document.querySelector('.posts');
-const postTemplate = document.getElementById('single-post');
-const form = document.querySelector('#new-post form');
-const fetchButton = document.querySelector('#available-posts button');
-const postList = document.querySelector('ul');
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+const form = document.querySelector("#new-post form");
+const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("ul");
 
 function sendHttpRequest(method, url, data) {
   // const promise = new Promise((resolve, reject) => {
@@ -34,38 +34,41 @@ function sendHttpRequest(method, url, data) {
   return fetch(url, {
     method: method,
     // body: JSON.stringify(data),
-    body: data
+    body: data,
     // headers: {
     //   'Content-Type': 'application/json'
     // }
   })
-    .then(response => {
+    .then((response) => {
       if (response.status >= 200 && response.status < 300) {
         return response.json();
       } else {
-        return response.json().then(errData => {
+        return response.json().then((errData) => {
           console.log(errData);
-          throw new Error('Something went wrong - server-side.');
+          throw new Error("Something went wrong - server-side.");
         });
       }
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(error);
-      throw new Error('Something went wrong!');
+      throw new Error("Something went wrong!");
     });
 }
+
+// axios 는 항상 프로미스를 반환함
+// axios 는 본문 내용을 파악하여 알아서(자동으로) 헤더와 content-type 을 설정함 + 데이터를 알맞게 변환
 
 async function fetchPosts() {
   try {
     const response = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
+      "https://jsonplaceholder.typicode.com/posts"
     );
     const listOfPosts = response.data;
     for (const post of listOfPosts) {
       const postEl = document.importNode(postTemplate.content, true);
-      postEl.querySelector('h2').textContent = post.title.toUpperCase();
-      postEl.querySelector('p').textContent = post.body;
-      postEl.querySelector('li').id = post.id;
+      postEl.querySelector("h2").textContent = post.title.toUpperCase();
+      postEl.querySelector("p").textContent = post.body;
+      postEl.querySelector("li").id = post.id;
       listElement.append(postEl);
     }
   } catch (error) {
@@ -79,33 +82,33 @@ async function createPost(title, content) {
   const post = {
     title: title,
     body: content,
-    userId: userId
+    userId: userId,
   };
 
   const fd = new FormData(form);
   // fd.append('title', title);
   // fd.append('body', content);
-  fd.append('userId', userId);
+  fd.append("userId", userId);
 
   const response = await axios.post(
-    'https://jsonplaceholder.typicode.com/posts',
+    "https://jsonplaceholder.typicode.com/posts",
     fd
   );
   console.log(response);
 }
 
-fetchButton.addEventListener('click', fetchPosts);
-form.addEventListener('submit', event => {
+fetchButton.addEventListener("click", fetchPosts);
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const enteredTitle = event.currentTarget.querySelector('#title').value;
-  const enteredContent = event.currentTarget.querySelector('#content').value;
+  const enteredTitle = event.currentTarget.querySelector("#title").value;
+  const enteredContent = event.currentTarget.querySelector("#content").value;
 
   createPost(enteredTitle, enteredContent);
 });
 
-postList.addEventListener('click', event => {
-  if (event.target.tagName === 'BUTTON') {
-    const postId = event.target.closest('li').id;
+postList.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    const postId = event.target.closest("li").id;
     axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`);
   }
 });
