8ab88683458cdcf071490e9f5e5829136a8aa6f9
MINJI <always0524@naver.com>
2025-11-17 15:31:59 +0900

    learn. webpack-dev-server 사용해보기

--- 21-JavaScript-Tools/webpack.config.js
+++ 21-JavaScript-Tools/webpack.config.js
@@ -1,8 +1,15 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/app.js",
   output: {
+    filename: "app.js",
     path: path.resolve(__dirname, "assets", "scripts"),
+    publicPath: "assets/scripts/",
   },
+  // 아래 devServer 내용은 별도로 설정할 필요가 없는 기본값임
+  //   devServer: {
+  //     contentBase: "./",
+  //   },
 };
