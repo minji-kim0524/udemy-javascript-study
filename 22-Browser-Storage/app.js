d32751ff42f043cf83e667dbf42338424b4fc7b8
MINJI <always0524@naver.com>
2025-11-18 15:49:30 +0900

    learn. localStorage 학습

---
+++ 22-Browser-Storage/app.js
@@ -0,0 +1,29 @@
+// 로컬 스토리지
+// 기본 데이터 저장하는 키-값 스토리지
+
+const storeBtn = document.getElementById("store-btn");
+const retrBtn = document.getElementById("retrieve-btn");
+
+const userId = "u123";
+const user = {
+  name: "Max",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+};
+
+storeBtn.addEventListener("click", () => {
+  localStorage.setItem("uid", userId);
+  localStorage.setItem("uid", JSON.stringify(user));
+  console.log("클릭됨");
+});
+
+retrBtn.addEventListener("click", () => {
+  const extractedId = localStorage.getItem("uid");
+  const extractedUser = JSON.parse(localStorage.getItem("user"));
+  console.log(extractedUser);
+  if (extractedId) {
+    console.log("Got the id -" + extractedId);
+  } else {
+    console.log("Could not find id");
+  }
+});
