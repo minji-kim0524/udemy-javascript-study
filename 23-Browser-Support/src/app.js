aa34c67a6f6e2af001d6f4d90729f02da018d835
MINJI <always0524@naver.com>
2025-11-19 15:32:04 +0900

    learn. 기능 탐지 + 폴백 코드

--- 23-Browser-Support/src/app.js
+++ 23-Browser-Support/src/app.js
@@ -1,5 +1,19 @@
-const button = document.querySelector('button');
+const button = document.querySelector("button");
+const textParagraph = document.querySelector("p");
 
-button.addEventListener('click', () => {
+button.addEventListener("click", () => {
+  const text = textParagraph.textContent;
+  if (navigator.clipboard) {
+    navigator.clipboard
+      .writeText(text)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } else {
+    alert("Feature not available, please copy manually");
+  }
   // do something...
 });
