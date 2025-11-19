2147d82e70a74f82bded5827089a98446ea2d4c3
MINJI <always0524@naver.com>
2025-11-19 17:14:28 +0900

    learn. 코드 트랜스파일링

--- 23-Browser-Support/webpack.config.prod.js
+++ 23-Browser-Support/webpack.config.prod.js
@@ -1,19 +1,32 @@
-const path = require('path');
-const CleanPlugin = require('clean-webpack-plugin');
+const path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/app.js',
+  mode: "production",
+  entry: "./src/app.js",
   output: {
-    filename: '[contenthash].js',
-    path: path.resolve(__dirname, 'assets', 'scripts'),
-    publicPath: 'assets/scripts/'
+    filename: "[contenthash].js",
+    path: path.resolve(__dirname, "assets", "scripts"),
+    publicPath: "assets/scripts/",
   },
-  devtool: 'cheap-source-map',
+  devtool: "cheap-source-map",
   // devServer: {
   //   contentBase: './'
   // }
-  plugins: [
-    new CleanPlugin.CleanWebpackPlugin()
-  ]
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            targets: "defaults",
+            presets: [["@babel/preset-env"]],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
