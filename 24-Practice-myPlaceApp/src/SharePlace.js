6b7876f0280f5467ca50cec85f59506eaacebeae
MINJI <always0524@naver.com>
2025-11-20 11:36:57 +0900

    learn. DOM 액세스 얻기

---
+++ 24-Practice-myPlaceApp/src/SharePlace.js
@@ -0,0 +1,14 @@
+// 객체 지향방법으로 진행
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector("form");
+    const locateUserBtn = document.getElementById("locate-btn");
+
+    locateUserBtn.addEventListener("click", this.locateUserHandler);
+    addressForm.addEventListener("submit", this.findAddressHandler);
+  }
+
+  locateUserHandler() {}
+
+  findAddressHandler() {}
+}
