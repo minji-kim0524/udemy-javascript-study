d810814fef1fca65a68ed1d236c4777f51582f0b
MINJI <always0524@naver.com>
2025-11-20 12:08:53 +0900

    learn. 사용자 위치 알아내기

--- 24-Practice-myPlaceApp/src/SharePlace.js
+++ 24-Practice-myPlaceApp/src/SharePlace.js
@@ -8,7 +8,31 @@ class PlaceFinder {
     addressForm.addEventListener("submit", this.findAddressHandler);
   }
 
-  locateUserHandler() {}
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert(
+        "Location feature is not available in your broswer - please use a more modern browser or manually enter an address"
+      );
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (successResult) => {
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
+        };
+        console.log(coordinates);
+      },
+      (error) => {
+        alert(
+          "Could not locate you unfortunately. Please enter an address manually!"
+        );
+      }
+    );
+  }
 
   findAddressHandler() {}
 }
+
+const placeFinder = new PlaceFinder();
