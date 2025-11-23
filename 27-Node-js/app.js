d9ce0e34885805af4438a4ccefd6375ebfbcef21
MINJI <always0524@naver.com>
2025-11-23 21:57:15 +0900

    learn. Express.js: 기초

--- 27-Node-js/app.js
+++ 27-Node-js/app.js
@@ -1,1 +1,15 @@
 // Express.js 를 사용하여 웹 서버 설정하기
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Content-Type", "text/html");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.listen(3000);
