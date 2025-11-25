24773728924ec1c96e475cb72554c1a71a4055e7
MINJI <always0524@naver.com>
2025-11-25 14:12:14 +0900

    learn. 기본 REST 경로 추가하기

--- 27-Node-js/app.js
+++ 27-Node-js/app.js
@@ -2,30 +2,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const locationRoutes = require("./routes/location");
+
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+// app.set("view engine", "ejs");
+// app.set("views", "views");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Content-Type", "text/html");
-  next();
-});
+app.use(locationRoutes);
 
-app.use((req, res, next) => {
-  const userName = req.body.username || "Unknown User";
-  //   if (body) {
-  //     userName = body.split("=")[1];
-  //   }
-  //   res.send(
-  //     `<h1>Hi ${userName}</h1><form method="POST" action="/"><input name="username" type="text"><button type="submit">Send</button></form>`
-  //   );
-  // res.send 를 ejs 를 이용한 구문으로 변경시 아래와 같이 작성할 수 있음
-  res.render("index", {
-    user: userName,
-  });
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Content-Type", "text/html");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   const userName = req.body.username || "Unknown User";
+//   if (body) {
+//     userName = body.split("=")[1];
+//   }
+//   res.send(
+//     `<h1>Hi ${userName}</h1><form method="POST" action="/"><input name="username" type="text"><button type="submit">Send</button></form>`
+//   );
+// res.send 를 ejs 를 이용한 구문으로 변경시 아래와 같이 작성할 수 있음
+// res.render("index", {
+//   user: userName,
+// });
+// });
 
 app.listen(3000);
---
+++ 27-Node-js/routes/location.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const router = express.Router();
+
+const locationStorage = {
+  locations: [],
+};
+
+// 첫번째 인수: 요청을 받을 위치
+// 두번째 인수: 받은 요청을 처리하는 함수
+router.post("/add-location", (req, res, next) => {
+  locationStorage.locations.push({
+    id: Math.random(),
+    address: req.body.address,
+    coords: { lat: req.body.lat, lng: req.body.lng },
+  });
+  res.json({ messgae: "Stored location!" });
+});
+
+router.get("/location", (req, res, next) => {});
+
+module.exports = router;
