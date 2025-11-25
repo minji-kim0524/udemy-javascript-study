d31c5279848c58039373369ac4904a57b64bac75
MINJI <always0524@naver.com>
2025-11-26 00:44:17 +0900

    learn. 교차-사이트 스크립팅 공격 (XSS)

--- 28-Security/src/MyPlace.js
+++ 28-Security/src/MyPlace.js
@@ -1,10 +1,13 @@
-import { Map } from './UI/Map';
-import sanitizeHtml from 'sanitize-html';
+import { Map } from "./UI/Map";
+import sanitizeHtml from "sanitize-html";
+
+// 안전성 검사(sanitize)
+// - 가능한 서버 측에서 이루어져야 함
 
 class LoadedPlace {
   constructor(coordinates, address) {
     new Map(coordinates);
-    const headerTitleEl = document.querySelector('header h1');
+    const headerTitleEl = document.querySelector("header h1");
     headerTitleEl.innerHTML = sanitizeHtml(address);
   }
 }
@@ -12,8 +15,8 @@ class LoadedPlace {
 const url = new URL(location.href);
 const queryParams = url.searchParams;
 const coords = {
-  lat: parseFloat(queryParams.get('lat')),
-  lng: +queryParams.get('lng')
+  lat: parseFloat(queryParams.get("lat")),
+  lng: +queryParams.get("lng"),
 };
-const address = queryParams.get('address');
+const address = queryParams.get("address");
 new LoadedPlace(coords, address);
