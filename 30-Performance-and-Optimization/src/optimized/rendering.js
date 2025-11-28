37939fef9f1737c3ad4d31f04a611368e9bc5b4c
MINJI <always0524@naver.com>
2025-11-28 15:40:34 +0900

    learn. DOM 올바르게 업데이트하기

--- 30-Performance-and-Optimization/src/optimized/rendering.js
+++ 30-Performance-and-Optimization/src/optimized/rendering.js
@@ -1,18 +1,20 @@
 export function renderProducts(products, deleteProductFn) {
-  const productListEl = document.getElementById('product-list');
-  productListEl.innerHTML = '';
-  products.forEach(product => {
-    const newListEl = document.createElement('li');
-    const prodTitleEl = document.createElement('h2');
-    const prodPriceEl = document.createElement('p');
-    const prodDeleteButtonEl = document.createElement('button');
+  const productListEl = document.getElementById("product-list");
+  productListEl.innerHTML = "";
+  products.forEach((product) => {
+    const newListEl = document.createElement("li");
+    const prodTitleEl = document.createElement("h2");
+    const prodPriceEl = document.createElement("p");
+    const prodDeleteButtonEl = document.createElement("button");
 
     prodTitleEl.innerHTML = product.title;
     prodPriceEl.innerHTML = product.price;
-    prodDeleteButtonEl.innerHTML = 'DELETE';
+    prodDeleteButtonEl.innerHTML = "DELETE";
+
+    newListEl.id = product.id;
 
     prodDeleteButtonEl.addEventListener(
-      'click',
+      "click",
       deleteProductFn.bind(null, product.id)
     );
 
@@ -22,4 +24,14 @@ export function renderProducts(products, deleteProductFn) {
 
     productListEl.appendChild(newListEl);
   });
+}
+
+export function updateProducts(product, prodId, deleteProductFn, isAdding) {
+  if (isAdding) {
+  } else {
+    const productEl = document.getElementById(prodId);
+    // 아래 두 가지 방법은 모두 작동함
+    productEl.remove(); // 모든 브라우저 작동하진 않지만 더 짧고 현대적인 접근 방법
+    // productEl.parentElement.removeChild(productEl); // 더 다양한 브라우저에서 작동하지만 레거시한 방법
+  }
 }
