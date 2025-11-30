476d16ee406bbbe2cafedb5c143d2a210f499235
MINJI <always0524@naver.com>
2025-11-30 23:49:44 +0900

    learn. 알고리즘 예시 1 - 최소값 구하기 (다른 방법 활용)

--- 33-Algorithm/alg-1-minimum.js
+++ 33-Algorithm/alg-1-minimum.js
@@ -17,6 +17,35 @@ function getMin(numbers) {
   return currentMinimum;
 }
 
+// 다른 방법 - 배열을 오름차순으로 정렬한 후 첫 번째 요소 반환하기
+function getMin2(numbers) {
+  if (!numbers.length) {
+    throw new Error("Should not be an empty array!");
+  }
+
+  let sortedNumbers;
+
+  for (let i = 0; i < numbers.length; i++) {
+    let outerElement = numbers[i];
+    for (let j = i + 1; j < numbers.length; j++) {
+      let innerElement = numbers[j];
+
+      if (outerElement > innerElement) {
+        // swap (자리바꾸기)
+        numbers[i] = innerElement;
+        numbers[j] = outerElement;
+
+        outerElement = numbers[i];
+        innerElement = numbers[j];
+
+        // console.log(numbers[i], numbers[j]);
+      }
+    }
+  }
+
+  return numbers[0];
+}
+
 const testNumbers = [3];
 
 const min = getMin(testNumbers);
