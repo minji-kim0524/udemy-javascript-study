ade7eba4e2623d5a0a758ad02742bbd6e77759a0
MINJI <always0524@naver.com>
2025-12-01 11:12:15 +0900

    learn. 성능 & “Big O” 표기법

--- 33-Algorithm/alg-1-minimum.js
+++ 33-Algorithm/alg-1-minimum.js
@@ -7,15 +7,25 @@ function getMin(numbers) {
   //     return numbers[0]
   // }
   let currentMinimum = numbers[0];
+  console.log("EXECUTION - INIT"); // 실행횟수 확인용
 
   for (let i = 1; i < numbers.length; i++) {
+    console.log("EXECUTION - FOR"); // 실행횟수 확인용
     if (numbers[i] < currentMinimum) {
       currentMinimum = numbers[i];
     }
   }
 
+  console.log("EXECUTION - RETURN"); // 실행횟수 확인용
   return currentMinimum;
 }
+
+// 알고리즘에서는 입력값에 따라 달라지는 부분이 중요함 << 성능에 영향을 미침
+// 실행횟수에 따라 총 3개의 블록으로 나눌 수 있음(if문 / for문 / return문)
+// T(time) = n (입력값에 따라 달라지는 부분의 실행횟수)
+// 위에서 횟수(n)는 선형적으로 배열의 길에 따라 달라짐 => '선형 시간 복잡도(Linear Time Complexity)' 라고함
+// - 즉, 더 많은 항목을 입력할수록 소요되는 시간의 증가폭이 선형적으로 늘어남
+// 일반적으로 이를 '빅 오(Big O) 표기법'으로 표시함 => 위 내용을 해당 표기법으로 나타내면 'O(n)' 으로 표기가능
 
 // 다른 방법 - 배열을 오름차순으로 정렬한 후 첫 번째 요소 반환하기
 function getMin2(numbers) {
