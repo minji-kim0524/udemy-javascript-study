b24f71fe0eb4fe2f7db75b766b920c9c7977b692
MINJI <always0524@naver.com>
2025-12-05 12:53:27 +0900

    learn. 상수 사용하기

--- 02-Basic-JavaScript/assets/scripts/app.js
+++ 02-Basic-JavaScript/assets/scripts/app.js
@@ -1,11 +1,14 @@
 // 계산기 로직
 
 // 계산결과 로직
-// 변수 선언
-let currentResult = 0;
+// 상수
+// - 변수를 수정해도 상수 원본과 값은 바뀌지 않음
+const defaultResult = 0;
+// 변수 선언 및 초기화
+let currentResult = defaultResult;
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-let calculationDescription = "(" + currentResult + "+ 10) * 3 / 2 -1";
+let calculationDescription = "(" + defaultResult + "+ 10) * 3 / 2 -1";
 
 outputResult(currentResult, calculationDescription);
