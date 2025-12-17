2f8401eeec6164edbd1cc253cbea67e9fd98502d
MINJI <always0524@naver.com>
2025-12-17 14:09:23 +0900

    learn. 프로퍼티 추가, 수정 & 삭제

--- 09-Objects/assets/scripts/objects.js
+++ 09-Objects/assets/scripts/objects.js
@@ -7,4 +7,13 @@ const person = {
   },
 };
 
+//새로운 프로퍼티 및 값 할당
+person.isAdmin = true;
+console.log(person);
+
+// 프로퍼티 삭제
+delete person.age;
+console.log(person);
+console.log(person.age); // undefined
+
 person.greet();
