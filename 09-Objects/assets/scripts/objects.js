26b8678ba9b91319fc348241c85d25a1083d81a2
MINJI <always0524@naver.com>
2025-12-19 15:08:38 +0900

    learn. 데모 앱 & 축약어 프로퍼티 구문

--- 09-Objects/assets/scripts/objects.js
+++ 09-Objects/assets/scripts/objects.js
@@ -1,39 +1,72 @@
-const movieList = document.getElementById("movie-list");
+// const movieList = document.getElementById("movie-list");
 
-movieList.style["background-color"] = "red";
-movieList.style.display = "block";
+// movieList.style["background-color"] = "red";
+// movieList.style.display = "block";
 
-const userChosenKeyName = "level";
+// const userChosenKeyName = "level";
 
-let person = {
-  "first name": "Max",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  [userChosenKeyName]: "...",
-  greet: function () {
-    alert("Hi there!");
-  },
-  1.5: "hello",
+// let person = {
+//   "first name": "Max",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   [userChosenKeyName]: "...",
+//   greet: function () {
+//     alert("Hi there!");
+//   },
+//   1.5: "hello",
+// };
+
+// //새로운 프로퍼티 및 값 할당
+// person.isAdmin = true;
+// console.log(person);
+
+// // 자바스크립트 객체의 프로퍼티 정렬 순서
+// // - 숫자: 오름차순
+// // - 문자열: 프로퍼티 추가한 순서
+
+// // 프로퍼티 삭제
+// delete person.age;
+// console.log(person);
+// console.log(person.age); // undefined
+
+// // person.greet();
+
+// const keyName = "first name";
+// console.log(person[keyName]); // ????
+
+// // 대괄호 표기법 -> 프로퍼티에 동적 액세스 가능
+// console.log(person["first name"]);
+// console.log(person[1.5]);
+
+// ----------------------------------------------------
+
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
+
+const movies = [];
+
+const addMovieHandler = () => {
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
+
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  )
+    return;
+
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue,
+    },
+    id: Math.random(),
+  };
+
+  movies.push(newMovie);
+  console.log(newMovie);
 };
 
-//새로운 프로퍼티 및 값 할당
-person.isAdmin = true;
-console.log(person);
-
-// 자바스크립트 객체의 프로퍼티 정렬 순서
-// - 숫자: 오름차순
-// - 문자열: 프로퍼티 추가한 순서
-
-// 프로퍼티 삭제
-delete person.age;
-console.log(person);
-console.log(person.age); // undefined
-
-// person.greet();
-
-const keyName = "first name";
-console.log(person[keyName]); // ????
-
-// 대괄호 표기법 -> 프로퍼티에 동적 액세스 가능
-console.log(person["first name"]);
-console.log(person[1.5]);
+addMovieBtn.addEventListener("click", addMovieHandler);
