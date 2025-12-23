adc05b3429355e8bfea91c6d6e257b8a2e813751
MINJI <always0524@naver.com>
2025-12-24 03:37:26 +0900

    learn. 다수의 클래스 사용하고 “연결”하기

--- 10-Object-Oriented-Programming/assets/scripts/app.js
+++ 10-Object-Oriented-Programming/assets/scripts/app.js
@@ -16,60 +16,116 @@ class Product {
   }
 }
 
-console.log(new Product());
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
 
-const productList = {
-  products: [
-    // 새로운 객체 생성
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+            <div>
+                <img src="${this.product.imageUrl}" alt="${this.product.title}">
+                <div class="product-item__content">
+                    <h2>${this.product.title}</h2>
+                    <h3>\$${this.product.price}</h3>
+                    <p>${this.product.description}}</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+          `;
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
     new Product(
       "A Pillow",
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlNPBR_0gPsRmSwOyHcdWdk8-LhSR-rx6csA&s",
       "A soft pillow!",
       19.99
     ),
-    // {
-    //   title: "A Pillow",
-    //   imageUrl:
-    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlNPBR_0gPsRmSwOyHcdWdk8-LhSR-rx6csA&s",
-    //   price: 19.99,
-    //   description: "A soft pillow!",
-    // },
     new Product(
       "A Carpet",
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLUBXEH_PVtgNjPc8NzeXl1MRSVK-bM5uHOQ&s",
       "A carpet which you migth like =- or not.",
       89.99
     ),
-    // {
-    //   title: "A Carpet",
-    //   imageUrl:
-    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLUBXEH_PVtgNjPc8NzeXl1MRSVK-bM5uHOQ&s",
-    //   price: 89.99,
-    //   description: "A carpet which you migth like =- or not.",
-    // },
-  ],
+  ];
+
+  constructor() {}
+
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.claasName = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-            <div>
-                <img src="${prod.imageUrl}" alt="${prod.title}">
-                <div class="product-item__content">
-                    <h2>${prod.title}</h2>
-                    <h3>\$${prod.price}</h3>
-                    <p>${prod.description}}</p>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-          `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
-  },
-};
+  }
+}
 
+const productList = new ProductList();
 productList.render();
+console.log(new Product());
+
+// const productList = {
+//   products: [
+// 새로운 객체 생성
+// new Product(
+//   "A Pillow",
+//   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlNPBR_0gPsRmSwOyHcdWdk8-LhSR-rx6csA&s",
+//   "A soft pillow!",
+//   19.99
+// ),
+// {
+//   title: "A Pillow",
+//   imageUrl:
+//     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlNPBR_0gPsRmSwOyHcdWdk8-LhSR-rx6csA&s",
+//   price: 19.99,
+//   description: "A soft pillow!",
+// },
+// new Product(
+//   "A Carpet",
+//   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLUBXEH_PVtgNjPc8NzeXl1MRSVK-bM5uHOQ&s",
+//   "A carpet which you migth like =- or not.",
+//   89.99
+// ),
+// {
+//   title: "A Carpet",
+//   imageUrl:
+//     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLUBXEH_PVtgNjPc8NzeXl1MRSVK-bM5uHOQ&s",
+//   price: 89.99,
+//   description: "A carpet which you migth like =- or not.",
+// },
+//   ],
+//   render() {
+//     const renderHook = document.getElementById("app");
+//     const prodList = document.createElement("ul");
+//     prodList.claasName = "product-list";
+//     for (const prod of this.products) {
+//       const prodEl = document.createElement("li");
+//       prodEl.className = "product-item";
+//       prodEl.innerHTML = `
+//             <div>
+//                 <img src="${prod.imageUrl}" alt="${prod.title}">
+//                 <div class="product-item__content">
+//                     <h2>${prod.title}</h2>
+//                     <h3>\$${prod.price}</h3>
+//                     <p>${prod.description}}</p>
+//                     <button>Add to Cart</button>
+//                 </div>
+//             </div>
+//           `;
+//       prodList.append(prodEl);
+//     }
+//     renderHook.append(prodList);
+//   },
+// };
+
+// 클래스 인스턴스화
