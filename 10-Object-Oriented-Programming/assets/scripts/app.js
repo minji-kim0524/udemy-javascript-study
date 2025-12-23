37ee5c3d892aa3ff3f1dcf3719db37810bceb0a3
MINJI <always0524@naver.com>
2025-12-23 16:31:03 +0900

    learn. OOP 코드로 시작하기

---
+++ 10-Object-Oriented-Programming/assets/scripts/app.js
@@ -0,0 +1,42 @@
+const productList = {
+  products: [
+    {
+      title: "A Pillow",
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlNPBR_0gPsRmSwOyHcdWdk8-LhSR-rx6csA&s",
+      price: 19.99,
+      description: "A soft pillow!",
+    },
+    {
+      title: "A Carpet",
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLUBXEH_PVtgNjPc8NzeXl1MRSVK-bM5uHOQ&s",
+      price: 89.99,
+      description: "A carpet which you migth like =- or not.",
+    },
+  ],
+  render() {
+    const renderHook = document.getElementById("app");
+    const prodList = document.createElement("ul");
+    prodList.claasName = "product-list";
+    for (const prod of this.products) {
+      const prodEl = document.createElement("li");
+      prodEl.className = "product-item";
+      prodEl.innerHTML = `
+            <div>
+                <img src="${prod.imageUrl}" alt="${prod.title}">
+                <div class="product-item__content">
+                    <h2>${prod.title}</h2>
+                    <h3>\$${prod.price}</h3>
+                    <p>${prod.description}}</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+          `;
+      prodList.append(prodEl);
+    }
+    renderHook.append(prodList);
+  },
+};
+
+productList.render();
