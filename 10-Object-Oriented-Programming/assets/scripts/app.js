13e00ae3467fe050e93354fa890c2c661c06531c
MINJI <always0524@naver.com>
2025-12-25 04:33:36 +0900

    learn. 상속 구현하기

--- 10-Object-Oriented-Programming/assets/scripts/app.js
+++ 10-Object-Oriented-Programming/assets/scripts/app.js
@@ -16,7 +16,34 @@ class Product {
   }
 }
 
-class ShoppingCart {
+class ElementAttribute {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+
+class Component {
+  constructor(renderHookId) {
+    this.hookId = renderHookId;
+  }
+
+  createRootElement(tag, cssClasses, attributes) {
+    const rootElement = document.createElement(tag);
+    if (cssClasses) {
+      rootElement.className = cssClasses;
+    }
+    if (attributes && attributes.length > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
+      }
+    }
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+  }
+}
+
+class ShoppingCart extends Component {
   items = [];
 
   set cartItems(value) {
@@ -34,6 +61,11 @@ class ShoppingCart {
     return sum;
   }
 
+  constructor(renderHookId) {
+    // 부모 클래스의 생성자 호출
+    super(renderHookId);
+  }
+
   addProduct(product) {
     const updatedItems = [...this.items];
     updatedItems.push(product);
@@ -47,14 +79,14 @@ class ShoppingCart {
   // }
 
   render() {
-    const cartEl = document.createElement("section");
+    const cartEl = this.createRootElement("section", "cart");
+
     cartEl.innerHTML = `
         <h2>Total: \$${0}</h2>
         <button>Order Now!</button>
       `;
-    cartEl.className = "cart";
+
     this.totalOutput = cartEl.querySelector("h2");
-    return cartEl;
   }
 }
 
@@ -121,13 +153,13 @@ class Shop {
   render() {
     const renderHook = document.getElementById("app");
 
-    this.cart = new ShoppingCart();
-    const cartEl = this.cart.render();
+    this.cart = new ShoppingCart("app");
+    this.cart.render();
     const productList = new ProductList();
     const prodListEl = productList.render();
     // console.log(new Product());
 
-    renderHook.append(cartEl);
+    // renderHook.append(cartEl);
     renderHook.append(prodListEl);
   }
 }
