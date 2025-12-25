c628361fb31964fe5858dd45afa3ba07b8306a42
MINJI <always0524@naver.com>
2025-12-25 21:13:01 +0900

    learn. 모든 곳에서 상속 사용하기

--- 10-Object-Oriented-Programming/assets/scripts/app.js
+++ 10-Object-Oriented-Programming/assets/scripts/app.js
@@ -90,8 +90,9 @@ class ShoppingCart extends Component {
   }
 }
 
-class ProductItem {
-  constructor(product) {
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
+    super(renderHookId);
     this.product = product;
   }
 
@@ -100,8 +101,8 @@ class ProductItem {
   }
 
   render() {
-    const prodEl = document.createElement("li");
-    prodEl.className = "product-item";
+    const prodEl = this.createRootElement("li", "product-item");
+
     prodEl.innerHTML = `
         <div>
           <img src="${this.product.imageUrl}" alt="${this.product.title}">
@@ -115,11 +116,10 @@ class ProductItem {
       `;
     const addCartButton = prodEl.querySelector("button");
     addCartButton.addEventListener("click", this.addToCart.bind(this));
-    return prodEl;
   }
 }
 
-class ProductList {
+class ProductList extends Component {
   products = [
     new Product(
       "A Pillow",
@@ -135,32 +135,30 @@ class ProductList {
     ),
   ];
 
-  constructor() {}
+  constructor(renderHookId) {
+    super(renderHookId);
+  }
 
   render() {
-    const prodList = document.createElement("ul");
-    prodList.claasName = "product-list";
+    const prodList = this.createRootElement("ul", "product-list", [
+      new ElementAttribute("id", "prod-list"),
+    ]);
     for (const prod of this.products) {
-      const productItem = new ProductItem(prod);
-      const prodEl = productItem.render();
-      prodList.append(prodEl);
+      const productItem = new ProductItem(prod, "prod-list");
+      productItem.render();
     }
-    return prodList;
   }
 }
 
 class Shop {
   render() {
-    const renderHook = document.getElementById("app");
-
     this.cart = new ShoppingCart("app");
     this.cart.render();
-    const productList = new ProductList();
-    const prodListEl = productList.render();
+    const productList = new ProductList("app");
+    productList.render();
     // console.log(new Product());
 
     // renderHook.append(cartEl);
-    renderHook.append(prodListEl);
   }
 }
 
